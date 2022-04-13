23b8e717986a3852457d2441e63deed3018105a6
Your Name <you@example.com>
2022-04-13 20:46:14 +0800

    fragment_counter.py

---
+++ Practical8/fragment_counter.py
@@ -0,0 +1,12 @@
+seq = "ATGCAATCGACTACGATCAATCGAGGGCCGAATTCBHYG"
+fragments = []
+import re
+while re.search("GAATTC", seq):
+    co = seq.find("GAATTC")
+    fragments.append(seq[:co+1])
+    seq = seq[co+1:]
+fragments.append(seq)
+print("%d fragments will formed if EcoRI enzyme is used"%(len(fragments)))
+    
+
+    
