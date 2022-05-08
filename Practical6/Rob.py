7c2628548981e96dc1897fb6aec431eea25bb3ac
Your Name <you@example.com>
2022-05-08 15:45:38 +0800

    Rob's score distribution

--- Practical6/Rob.py
+++ Practical6/Rob.py
@@ -17,7 +17,8 @@ else:
 from matplotlib import pyplot as plt
 plt.boxplot(a, vert = True, whis = 1.5,  patch_artist = True, boxprops = {"color" : "red"}, showmeans = True, showbox = True, showcaps = True, showfliers = True, notch = False)#make boxplot and give requirements to the plot
 plt.title("Rob's score distribution", color = "green")#define the title
-plt.xlabel("score", color = "purple")#define what will be shown in x axis
+plt.xlabel("Rob", color = "purple")#define what will be shown in x axis
+plt.ylabel("score", color = "purple")#define what will be shown in y axis
 plt.show()#show the plot
 
 
