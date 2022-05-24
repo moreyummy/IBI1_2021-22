4c63e4c2560b9a03aaf94214d3e49f92733140f9
Your Name <you@example.com>
2022-05-24 20:27:57 +0800

    Practical10

---
+++ Practical10/ZJE_human_resources_databases.py
@@ -0,0 +1,10 @@
+class Staff(object):
+    def __init__(self, f, l, h, r):
+        self.first = f
+        self.last = l
+        self.home = h
+        self.role = r
+    def __str__(self):
+        return("%s %s  %s  %s"%(self.first, self.last, self.home, self.role))
+
+print(Staff('Li', 'Jackon', 'queen_road', 'leader'))
